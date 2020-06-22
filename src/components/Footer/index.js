74f8f598878f9baa1f02b75f801c0ce148a4befe
marcoaflg <marcosl19@gmail.com>
2020-06-22 15:56:39 -0300

    adding footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './styles.css';
+
+function Footer () {
+
+    return(
+        <footer>
+            <div className="content">
+                <div className="container flex">
+                    <ul id="menu" className="links flex">
+                        <li><a href="/"></a></li>
+                        <li><a href="/"></a></li>
+                        <li><a href="/"></a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 function Home() {
     return (
         <div className="Home">
             <Header />
+            <Footer />
         </div>
     );
 }
